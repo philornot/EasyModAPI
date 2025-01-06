b202cbfcc7af6b0d2b51ca1ecf943b91cb88ab93
philornot <philornot@users.noreply.github.com>
2025-01-06 18:49:52 +0100

    build kinda works now too

--- build.py
+++ build.py
@@ -3,10 +3,21 @@ build.py - Script do budowania aplikacji
 """
 import os
 import shutil
+from pathlib import Path
+
 import PyInstaller.__main__
+import tkinterdnd2
+
 from src.logger import setup_logger
 
 logger = setup_logger("ForestModManagerBuild")
+
+
+def get_tkdnd_path():
+    """Znajduje ścieżkę do biblioteki tkdnd"""
+    tkdnd_path = Path(tkinterdnd2.__file__).parent / 'tkdnd'
+    logger.info(f"Found tkdnd path: {tkdnd_path}")
+    return tkdnd_path
 
 
 def clean_dist():
@@ -17,7 +28,11 @@ def clean_dist():
 
 def create_spec():
     logger.info("Creating .spec file")
-    SPEC_CONTENT = '''# -*- mode: python ; coding: utf-8 -*-
+    tkdnd_path = get_tkdnd_path()
+
+    SPEC_CONTENT = f'''# -*- mode: python ; coding: utf-8 -*-
+
+block_cipher = None
 
 a = Analysis(
     ['src/app.py'],
@@ -28,21 +43,26 @@ a = Analysis(
         ('assets/fonts/*.ttf', 'assets/fonts'),
         ('src/ui', 'src/ui'),
         ('src/__init__.py', 'src'),
+        (r'{tkdnd_path}', 'tkinterdnd2/tkdnd'),
     ],
-    hiddenimports=['customtkinter', 'src.ui'],
+    hiddenimports=['customtkinter', 'src.ui', 'tkinterdnd2'],
     hookspath=[],
-    hooksconfig={},
+    hooksconfig={{}},
     runtime_hooks=[],
     excludes=[],
+    win_no_prefer_redirects=False,
+    win_private_assemblies=False,
+    cipher=block_cipher,
     noarchive=False,
 )
 
-pyz = PYZ(a.pure)
+pyz = PYZ(a.pure, a.zipped_data, cipher=block_cipher)
 
 exe = EXE(
     pyz,
     a.scripts,
     a.binaries,
+    a.zipfiles,
     a.datas,
     [],
     name='ForestModManager',
